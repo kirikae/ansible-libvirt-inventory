8431a3faf5fc9f442b1ee12a4d74b94a3c0d82ec
Kirikae <22813189+kirikae@users.noreply.github.com>
2019-02-27 16:03:25 +1100

    Added initial python script. amended README.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ansible-libvirt-inventory
 Ansible Dynamic Inventory plugin for libvirt (WIP)
+
+
+NOTE: in development.
+
+End goal is to have an inventory plugin for Ansible to use libvirt (remote or local hypervisor) as an inventory source
---
+++ ansible_libvirt.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+import libvirt
+from xml.dom import minidom
+
+conn = libvirt.open('qemu:///system')
+
+if conn == None:
+    print('Failed to connect to hypervisor', file=sys.stderr)
+    exit(1)
+
+domains = conn.listAllDomains(0)
+if len(domains) == 0:
+    print('No Domains found')
+else:
+    for domain in domains:
+
+        print('DOMAIN: ' + domain.name())
+        ifaces = domain.interfaceAddresses(0) # Currently only retieves first IP, not all...
+        for (name, val) in ifaces.iteritems():
+            if val['addrs']:
+                for ipaddr in val['addrs']:
+                    if ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV4:
+                        ip4 = ipaddr['addr']
+                    elif ipaddr['type'] == ibvirt.VIR_IP_ADDR_TYPE_IPV6:
+                        ipv6 = ipaddr['addr']
+                    print('IPv4 Address: ' + ip4)
+                    #print('IPv6 Address: ' + ip6 # Need a way to ignore ipv6 if no ipv6 address present
+
+conn.close()
+
